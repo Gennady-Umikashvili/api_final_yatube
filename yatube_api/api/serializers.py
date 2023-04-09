a2b87b5949461ad57a3308e1d61c78abc17f3a9a
Gennadyu Umikashvili <119798192+Gennady-Umikashvili@users.noreply.github.com>
2023-04-09 17:38:45 +0300

    Add files via upload

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,58 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, User, Follow
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = SlugRelatedField(slug_field="username", read_only=True)
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+    author = SlugRelatedField(read_only=True, slug_field="username")
+    post = PrimaryKeyRelatedField(read_only=True, many=False)
+
+    class Meta:
+        fields = "__all__"
+        model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = "__all__"
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = SlugRelatedField(
+        read_only=True,
+        slug_field="username",
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(), slug_field="username"
     )
 
     class Meta:
-        fields = '__all__'
-        model = Comment
+        fields = ("user", "following")
+        model = Follow
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=["user", "following"],
+                message="Уже подписаны!",
+            )
+        ]
+
+    def validate(self, data):
+        if self.context["request"].user == data["following"]:
+            raise serializers.ValidationError(
+                "Подписка на самого себя недоступна!"
+            )
+        return data
